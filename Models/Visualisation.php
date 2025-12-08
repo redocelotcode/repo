55f45cd5fa7fc1e275158466c324827bd17bc342
rjlewis1 <r.j.lewis1@edu.salford.ac.uk>
2025-12-08 12:33:30 +0000

    giving k example code

--- Models/Visualisation.php
+++ Models/Visualisation.php
@@ -1,4 +1,26 @@
 <?php
+require_once("../index.php");
+
 /**
  * K thats your job lol -RL
+ *
+ * put the example code below but you'll need to install phpchart
+ * might flag a virus but it seems to be one with a high false-positive rate
  */
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>phpChart - A Basic Chart</title>
+</head>
+<body>
+
+<?php
+$pc = new C_PhpChartX(array(array(11, 9, 5, 12, 14)),'basic_chart');
+$pc->draw();
+?>
+
+</body>
+</html>
