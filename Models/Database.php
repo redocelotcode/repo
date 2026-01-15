5bdc3a7328f7e7c4abae06bb4cc434765c8c397a
josh <o.j.abegunde@edu.salford.ac.uk>
2026-01-15 04:44:16 +0000

    graph generation

--- Models/Database.php
+++ Models/Database.php
@@ -22,7 +22,9 @@ class Database
     private function __construct()
     {
         try {
-            $this->_dbHandle = new PDO("sqlite:db/db.sqlite");
+                $this->_dbHandle = new \PDO("pgsql:host=localhost;port=5432;dbname=hackcamp",
+                "postgres",
+                "*PUTYOURPASSWORD");
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
